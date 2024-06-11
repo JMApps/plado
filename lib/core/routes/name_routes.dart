1fa0f0fa5296704e553aa8d4f368d3925af3508f
jmapps <jmappsmuslim@gmail.com>
2024-06-11 17:01:20 +0300

    Added first route names

---
+++ lib/core/routes/name_routes.dart
@@ -0,0 +1,7 @@
+class NameRoutes {
+  static const String createTaskPage = '/add_task_page';
+  static const String updateTaskPage = '/update_task_page';
+
+  static const String createHabitPage = '/create_habit_page';
+  static const String updateHabitPage = '/update_habit_page';
+}
