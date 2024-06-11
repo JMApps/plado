ddc5851fa6bfdfab86cb317d473a3ad3a1bf4714
jmapps <jmappsmuslim@gmail.com>
2024-06-11 16:06:28 +0300

    Added AppExceptionMessages class

---
+++ lib/core/strings/app_exception_messages.dart
@@ -0,0 +1,9 @@
+class AppExceptionMessages {
+  static const String dateIsAfterException = 'Start date must be before end date';
+  static const String getAllTaskException = 'Error get all task\n';
+  static const String getTaskByIdException = 'Error get task by id\n';
+  static const String getTasksByModeException = 'Error get task by mode\n';
+  static const String createTaskException = 'Error create task\n';
+  static const String updateTaskException = 'Error update task\n';
+  static const String deleteTaskException = 'Error remove task\n';
+}
