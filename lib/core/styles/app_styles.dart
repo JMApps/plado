ec9e72b81d6007dc8093166da8fd518be72c48a9
jmapps <jmappsmuslim@gmail.com>
2024-06-13 21:21:05 +0300

    Added app styles

---
+++ lib/core/styles/app_styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const padding = EdgeInsets.all(14);
+  static const paddingMini = EdgeInsets.all(7);
+
+  static const paddingHorizontal = EdgeInsets.symmetric(horizontal: 14);
+  static const paddingHorizontalMini = EdgeInsets.symmetric(horizontal: 7);
+
+  static const paddingVertical = EdgeInsets.symmetric(horizontal: 14);
+  static const paddingVerticalMini = EdgeInsets.symmetric(horizontal: 7);
+
+  static const paddingTop = EdgeInsets.only(top: 14);
+  static const paddingTopMini = EdgeInsets.only(top: 7);
+
+  static const paddingRight = EdgeInsets.only(right: 14);
+  static const paddingRightMini = EdgeInsets.only(right: 7);
+
+  static const paddingBottom = EdgeInsets.only(bottom: 14);
+  static const paddingBottomMini = EdgeInsets.only(bottom: 7);
+
+  static const paddingLeft = EdgeInsets.only(left: 14);
+  static const paddingLeftMini = EdgeInsets.only(left: 7);
+
+  static const border = BorderRadius.all(Radius.circular(14));
+  static const borderMini = BorderRadius.all(Radius.circular(7));
+}
