f5aa3a24a8c505be4111e0a0f29b7f3d86f23cf5
jmapps <jmappsmuslim@gmail.com>
2024-06-18 22:39:29 +0300

    Added main app font

--- lib/core/themes/app_material_styles.dart
+++ lib/core/themes/app_material_styles.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../styles/app_styles.dart';
+
 class AppMaterialStyles {
   static ThemeData lightTheme = ThemeData(
+    fontFamily: 'Raleway',
     colorScheme: ColorScheme.fromSeed(
       brightness: Brightness.light,
       seedColor: Colors.orange,
     ),
     appBarTheme: const AppBarTheme(
       centerTitle: true,
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      contentPadding: AppStyles.paddingHorizontal,
+      alignLabelWithHint: true,
+      floatingLabelAlignment: FloatingLabelAlignment.center,
+      border: OutlineInputBorder(
+        borderRadius: AppStyles.border,
+        borderSide: BorderSide(
+          width: 0.5,
+        ),
+      ),
     ),
     bottomSheetTheme: const BottomSheetThemeData(
       showDragHandle: true,
@@ -16,12 +30,24 @@ class AppMaterialStyles {
   );
 
   static ThemeData darkTheme = ThemeData(
+    fontFamily: 'Raleway',
     colorScheme: ColorScheme.fromSeed(
       brightness: Brightness.dark,
       seedColor: Colors.orange,
     ),
     appBarTheme: const AppBarTheme(
       centerTitle: true,
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      contentPadding: AppStyles.paddingHorizontal,
+      alignLabelWithHint: true,
+      floatingLabelAlignment: FloatingLabelAlignment.center,
+      border: OutlineInputBorder(
+        borderRadius: AppStyles.border,
+        borderSide: BorderSide(
+          width: 0.5,
+        ),
+      ),
     ),
     bottomSheetTheme: const BottomSheetThemeData(
       showDragHandle: true,
