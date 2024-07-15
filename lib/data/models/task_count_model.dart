bf5aae78e9e25813890bc96cdcf5617ae29c9f76
jmapps <jmappsmuslim@gmail.com>
2024-07-15 15:34:36 +0300

    Added task count model

---
+++ lib/data/models/task_count_model.dart
@@ -0,0 +1,11 @@
+class TaskCountModel {
+  final int inProgress;
+  final int complete;
+  final int canceled;
+
+  TaskCountModel({
+    required this.inProgress,
+    required this.complete,
+    required this.canceled,
+  });
+}
