594d36990425a45298acb6554bed17ed0b41e475
jmapps <jmappsmuslim@gmail.com>
2024-08-07 23:49:48 +0300

    Added main graphic item

---
+++ lib/presentation/graphic/items/main_graphic_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainGraphicItem extends StatefulWidget {
+  const MainGraphicItem({super.key});
+
+  @override
+  State<MainGraphicItem> createState() => _MainGraphicItemState();
+}
+
+class _MainGraphicItemState extends State<MainGraphicItem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
