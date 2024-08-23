eb6b07cb7954377af301b7b2993bf263a61b30d0
jmapps <jmappsmuslim@gmail.com>
2024-08-23 20:18:16 +0300

    Added new import and export logic

--- lib/data/state/backup_state.dart
+++ lib/data/state/backup_state.dart
@@ -1,12 +1,30 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../../core/strings/app_constraints.dart';
 
 class BackupState extends ChangeNotifier {
 
   Future<void> exportBackupFile() async {
+    String databasesPath = await getDatabasesPath();
+    String dbPath = join(databasesPath, AppConstraints.dbName);
 
+    String formattedDate = DateTime.now().toString().replaceAll(':', '-');
+    String newFileName = '${basenameWithoutExtension(dbPath)}_$formattedDate.db';
+
+    final XFile dbFile = XFile(dbPath);
+    await Share.shareXFiles([dbFile], text: 'Резервное копирование');
   }
 
-  Future<void> importBackupFile() async {
-
+  Future<String?> importBackupFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      String? filePath = result.files.single.path;
+      // Copy from filePath with check
+    }
+    return result!.files.single.path;
   }
 }
--- lib/presentation/backup/backup_detail_page.dart
+++ lib/presentation/backup/backup_detail_page.dart
@@ -1,10 +1,5 @@
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:plado/core/strings/app_constraints.dart';
 import 'package:provider/provider.dart';
-import 'package:share_plus/share_plus.dart';
-import 'package:sqflite/sqflite.dart';
 
 import '../../core/strings/app_strings.dart';
 import '../../core/styles/app_styles.dart';
@@ -34,14 +29,7 @@ class BackupDetailPage extends StatelessWidget {
                 children: [
                   ListTile(
                     onTap: () async {
-                      String databasesPath = await getDatabasesPath();
-                      String dbPath = join(databasesPath, AppConstraints.dbName);
 
-                      String formattedDate = DateTime.now().toString().replaceAll(':', '-');
-                      String newFileName = '${basenameWithoutExtension(dbPath)}_$formattedDate.db';
-                      
-                      final XFile dbFile = XFile(dbPath);
-                      await Share.shareXFiles([dbFile], text: 'Резервное копирование');
                     },
                     shape: AppStyles.shape,
                     title: const Text(AppStrings.import),
@@ -49,11 +37,7 @@ class BackupDetailPage extends StatelessWidget {
                   ),
                   ListTile(
                     onTap: () async {
-                      FilePickerResult? result = await FilePicker.platform.pickFiles();
-                      if (result != null) {
-                        String? filePath = result.files.single.path;
-                        // Copy from filePath with check
-                      }
+
                     },
                     shape: AppStyles.shape,
                     title: const Text(AppStrings.export),
